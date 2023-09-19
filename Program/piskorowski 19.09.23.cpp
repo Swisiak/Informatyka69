2b18affe3ff0a8f4e1009d239f1f099635f9d023
Jarosław Gałecki <jarekgalecki6@gmail.com>
2023-09-19 14:00:37 +0200

    Program

---
+++ Program/piskorowski 19.09.23.cpp
@@ -0,0 +1,21 @@
+#include <iostream> 
+using namespace std;
+
+int main(){
+int x; 
+cout << "Wybierz liczbe ";
+cin >> a; 
+cout << "wpisz drugą liczbe" << x;
+
+int a, b;
+int sum;
+cout << "a";
+cin >> a;
+cout << "b";
+cin >> b;
+if (a>b)
+sum = x + y;
+cout << "odejmowanie od a zmienna b" << x;
+cout << "Wyświetl wynik odejmowania " << sum;
+    return 0;
+}
